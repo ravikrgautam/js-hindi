2788380862fd50acae334946d85a8e37286f7dc2
Ravi Kumar Gautam <54319773+ravikrgautam@users.noreply.github.com>
2024-06-07 11:09:59 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "ravi@farefuels.com"
+var accountPass = "12345"
+accountCity = "Gurugram"
+
+//accountId = 23123
+//console.log(accountId);
+
+accountEmail = "ravi@grandgetways.com"
+accountPass = "54321"
+accountCity = "Meerut"
+
+console.table([accountId, accountEmail, accountPass, accountCity]);
