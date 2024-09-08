ef8065ab128e5fc738e1acd3a6955f2433f87c8f
Ravi Kumar Gautam <54319773+ravikrgautam@users.noreply.github.com>
2024-09-08 13:23:14 +0000

    numbers

---
+++ 01_basics/06_nums_math.js
@@ -0,0 +1,20 @@
+// Numbers concept start here ********************
+
+const score = 100
+// console.log(score); //one way
+
+const balance = new Number(400)
+// console.log(balance); //2nd way
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(4));
+
+const newNumber = 23.8964
+const newNumber1 = 1223.2564
+const newNumber2 = 10000000
+// console.log(newNumber.toPrecision(3));
+// console.log(newNumber1.toPrecision(4));
+// console.log(newNumber2.toLocaleString('en-IN'));
+
+
+// Math concept start here ********************
