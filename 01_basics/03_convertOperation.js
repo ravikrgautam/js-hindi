1225f65bf1fb5f96d52d9182701e571af1d7e3ae
Ravi Kumar Gautam <54319773+ravikrgautam@users.noreply.github.com>
2024-09-08 10:23:39 +0000

    convert data types

---
+++ 01_basics/03_convertOperation.js
@@ -0,0 +1,20 @@
+let score = 33
+let score1 = "33abc"
+let score2 = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number (score1)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let a = "ravi"
+let b = Boolean(a);
+// console.log(b);
+// console.log(typeof b);
+
+let x = 33
+let stringNumber = String(x)
+console.log(stringNumber);
+console.log(typeof stringNumber);
