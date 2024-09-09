529087b80cba9c2f927064bb27ffa0cb3acbd5e5
Ravi Kumar Gautam <54319773+ravikrgautam@users.noreply.github.com>
2024-09-09 05:47:16 +0000

    numbers and math functions

--- 01_basics/06_nums_math.js
+++ 01_basics/06_nums_math.js
@@ -18,3 +18,20 @@ const newNumber2 = 10000000
 
 
 // Math concept start here ********************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.8));
+// console.log(Math.floor(4.8));
+// console.log(Math.ceil(4.8));
+// console.log(Math.min(2,5,8,9,6,5));
+// console.log(Math.max(2,5,8,9,6,5));
+
+// console.log(Math.random());
+// console.log(Math.random() * 10);
+// console.log((Math.random() * 10) + 1);
+// console.log(Math.floor((Math.random() * 10) + 1));
+
+const min = 1
+const max = 6
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
