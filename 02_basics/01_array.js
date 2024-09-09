c4bc5e5ff89ad54e9985e59d21551f954cee73d0
Ravi Kumar Gautam <54319773+ravikrgautam@users.noreply.github.com>
2024-09-09 09:07:50 +0000

    Array

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,3 +1,42 @@
-const newArr = Array [0,1,2,3,6,5,7,8]
-console.log(newArr);
+const newArr = [0,1,2,4,5,7,8]
+// console.log(newArr);
+// console.log(newArr[1]);
+
+const newArr2 = new Array (0,9,2,6,4,8)
+// console.log(newArr2[5]);
+
+// Array Methods *************************************************
+
+newArr.push(10)  // add a value in the end of array
+// console.log(newArr);
+
+newArr.pop(10)  // remove a value in the end of array
+// console.log(newArr);
+
+newArr.unshift(33) // add a value at the start of array
+// console.log(newArr);
+
+newArr.shift(33)  // remove a value at the Start of array
+// console.log(newArr);
+
+// console.log(newArr.includes(9)); // when we need to know value is exit or not
+// console.log(newArr.indexOf(9));  // when we need to know index position of value in array
+// console.log(newArr2.indexOf(9)); 
+
+// const myArr = newArr.join()
+// console.log(newArr);
+// console.log(myArr);
+// console.log(typeof newArr);
+// console.log(typeof myArr);
+
+const newArray = [0,4,5,7,8]
+console.log("A - ", newArray);
+
+const a1 = newArray.slice(0,3)
+console.log("B", a1);
+console.log("After Slice method", newArray);
+
+const a2 = newArray.splice(0,3)
+console.log("C", a2);
+console.log("After Splice method", newArray);
 
