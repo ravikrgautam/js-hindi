ada01e5abc191ce6ae72fd8b858aab17e8f9ae4d
Ravi Kumar Gautam <54319773+ravikrgautam@users.noreply.github.com>
2024-09-09 07:14:02 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,30 @@
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.toString());
+// console.log(newDate.toDateString());
+// console.log(newDate.toLocaleString());
+// console.log(newDate.toISOString());
+// console.log(newDate.toJSON());
+
+// console.log(typeof newDate); // object 
+
+let createDate = new Date(1997, 9, 11)
+// console.log(createDate.toDateString());
+// console.log(createDate.toLocaleString());
+
+let createDate1 = new Date(1997, 9, 11, 5, 30, 52)
+// console.log(createDate1.toLocaleString());
+
+// Timestamp ******************************************************************
+
+let NewDate1 = new Date("2024-09-09")
+let myTimeStamp = Date.now()
+// console.log(Date.now());
+// console.log(myTimeStamp);
+// console.log(NewDate1.getTime());
+// console.log(Date.now() / 1000)
+// console.log(Math.round(Date.now() / 1000))
+
+
+let newDate2 = new Date()
+console.log(newDate2);
