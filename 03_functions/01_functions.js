4c33c3a019b3c9dc2564ae2eecf504302a547ec5
Ravi Kumar Gautam <54319773+ravikrgautam@users.noreply.github.com>
2024-09-11 10:50:40 +0000

    functions

---
+++ 03_functions/01_functions.js
@@ -0,0 +1,14 @@
+// function addNUmbers (num1, num2){
+//     console.log(num1+num2);
+// }
+// addNUmbers(5, 6)
+
+
+function addNUmbers (num1, num2){
+    // let result = num1 + num2
+    // return result
+
+    return num1 + num2
+}
+const result = addNUmbers(5, 6)
+console.log("Result : ", result);
