b66e1edb256c7d681a62c29f7f5e07e841fadd86
Ravi Kumar Gautam <54319773+ravikrgautam@users.noreply.github.com>
2024-09-12 11:02:40 +0000

    control flow

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -7,20 +7,31 @@
 //     console.log("try again");
 // }
 
-const balance = 1000
+// let userLogIN = true
+// let userDebitCard = false
 
-if(balance <= 100){
-    console.log("100 rs in balance");
-} 
-else if (balance <= 500){
-    console.log("500 rs in balance");
-}
-else if (balance <= 700){
-    console.log("700 rs in balance");
-}
-else if (balance <= 900){
-    console.log("900 rs in balance");
-}
-else{
-    console.log("1000 rs in balance");
-}
+// if (userDebitCard && userLogIN){
+//     console.log("allow to buy");
+// } else{
+//     console.log("please enter correct details");
+// }
+
+
+// const balance = 1000
+
+// if(balance <= 100){
+//     console.log("100 rs in balance");
+// } 
+// else if (balance <= 500){
+//     console.log("500 rs in balance");
+// }
+// else if (balance <= 700){
+//     console.log("700 rs in balance");
+// }
+// else if (balance <= 900){
+//     console.log("900 rs in balance");
+// }
+// else{
+//     console.log("1000 rs in balance");
+// }
+
