5d4895020ed8aa941357d6fe47c15295570947de
Ravi Kumar Gautam <54319773+ravikrgautam@users.noreply.github.com>
2024-09-12 10:19:16 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,26 @@
+// let userEmail = true
+// let userGoogle = false
+
+// if (userEmail || userGoogle){
+//     console.log("log in successfully");
+// } else{
+//     console.log("try again");
+// }
+
+const balance = 1000
+
+if(balance <= 100){
+    console.log("100 rs in balance");
+} 
+else if (balance <= 500){
+    console.log("500 rs in balance");
+}
+else if (balance <= 700){
+    console.log("700 rs in balance");
+}
+else if (balance <= 900){
+    console.log("900 rs in balance");
+}
+else{
+    console.log("1000 rs in balance");
+}
